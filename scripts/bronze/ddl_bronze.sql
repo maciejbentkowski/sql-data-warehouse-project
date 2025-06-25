09483ac7803c8d6a7b103d9849349f41a40e32c5
Maciej Bentkowski <64644425+maciejbentkowski@users.noreply.github.com>
2025-06-25 21:00:20 +0200

    Create ddl_bronze.sql

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,67 @@
+/*
+=============================================================
+DDL Script: Create Bronze Tables
+=============================================================
+Script Purpose:
+    This script creates tables in the 'Bronze' schema, dropping existing tables if they already exist.
+
+WARNING:
+    Make sure the database and schemas has been created first.
+*/
+
+DROP TABLE IF EXISTS bronze.crm_cust_info;
+CREATE TABLE bronze.crm_cust_info (
+	  cst_id INTEGER,
+    cst_key VARCHAR(50),
+    cst_firstname VARCHAR(50),
+    cst_lastname VARCHAR(50),
+    cst_material_status VARCHAR(50),
+    cst_gndr VARCHAR(50),
+    cst_create_date DATE
+);
+
+DROP TABLE IF EXISTS bronze.crm_prd_info;
+CREATE TABLE bronze.crm_prd_info (
+    prd_id INTEGER,
+    prd_key VARCHAR(50),
+    prd_nm TEXT,
+    prd_cost INTEGER,
+    prd_line VARCHAR(50),
+    prd_start_dt DATE,
+    prd_end_dt DATE
+);
+
+DROP TABLE IF EXISTS bronze.crm_sales_details;
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num VARCHAR(50),
+    sls_prd_key VARCHAR(50),
+    sls_cust_id INTEGER,
+    sls_order_dt VARCHAR(50),
+    sls_ship_dt VARCHAR(50),
+    sls_due_dt VARCHAR(50),
+	  sls_sales INTEGER,
+    sls_quantity INTEGER,
+    sls_price INTEGER
+);
+
+DROP TABLE IF EXISTS bronze.erp_cust_az12;
+CREATE TABLE bronze.erp_cust_az12 (
+    cid VARCHAR(50),
+    bdate DATE,
+    gen VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS bronze.erp_loc_a101;
+CREATE TABLE bronze.erp_loc_a101 (
+	  cid VARCHAR(50),
+    cntry VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id VARCHAR(50),
+    cat VARCHAR(50),
+    subcat VARCHAR(50),
+    maintenance VARCHAR(50)
+);
+
