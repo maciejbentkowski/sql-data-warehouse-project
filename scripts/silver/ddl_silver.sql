dc5c8f3c404ba5429368215ac152be54975eadf4
Maciej Bentkowski <64644425+maciejbentkowski@users.noreply.github.com>
2025-06-30 11:59:05 +0200

    Update ddl_silver.sql
    Changed 
    sls_order_dt,
    sls_ship_dt,
    sls_due_dt
    columns DATATYPE to integer in silver.crm_sales_details table

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -11,26 +11,27 @@ WARNING:
 
 DROP TABLE IF EXISTS silver.crm_cust_info;
 CREATE TABLE silver.crm_cust_info (
-	cst_id INTEGER,
+    cst_id INTEGER,
     cst_key VARCHAR(50),
     cst_firstname VARCHAR(50),
     cst_lastname VARCHAR(50),
     cst_marital_status VARCHAR(50),
     cst_gndr VARCHAR(50),
     cst_create_date DATE,
-	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS silver.crm_prd_info;
 CREATE TABLE silver.crm_prd_info (
     prd_id INTEGER,
+    cat_id VARCHAR(50),
     prd_key VARCHAR(50),
     prd_nm TEXT,
     prd_cost INTEGER,
     prd_line VARCHAR(50),
     prd_start_dt DATE,
     prd_end_dt DATE,
-	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS silver.crm_sales_details;
@@ -38,13 +39,13 @@ CREATE TABLE silver.crm_sales_details (
     sls_ord_num VARCHAR(50),
     sls_prd_key VARCHAR(50),
     sls_cust_id INTEGER,
-    sls_order_dt VARCHAR(50),
-    sls_ship_dt VARCHAR(50),
-    sls_due_dt VARCHAR(50),
-	sls_sales INTEGER,
+    sls_order_dt INTEGER,
+    sls_ship_dt INTEGER,
+    sls_due_dt INTEGER,
+    sls_sales INTEGER,
     sls_quantity INTEGER,
     sls_price INTEGER,
-	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS silver.erp_cust_az12;
@@ -52,14 +53,14 @@ CREATE TABLE silver.erp_cust_az12 (
     cid VARCHAR(50),
     bdate DATE,
     gen VARCHAR(50),
-	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS silver.erp_loc_a101;
 CREATE TABLE silver.erp_loc_a101 (
-	cid VARCHAR(50),
+    cid VARCHAR(50),
     cntry VARCHAR(50),
-	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS silver.erp_px_cat_g1v2;
@@ -68,5 +69,5 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     cat VARCHAR(50),
     subcat VARCHAR(50),
     maintenance VARCHAR(50),
-	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
